3ad8e8eceb93c437f905ace7f781acdbfa18f38d
Almany04 <bencealmas@gmail.com>
2025-06-18 05:13:14 +0200

    fixes!

--- ChatApp.Endpoint/Controllers/ChatController.cs
+++ ChatApp.Endpoint/Controllers/ChatController.cs
@@ -1,6 +1,42 @@
 ﻿using ChatApp.Endpoint.Services;
 using Microsoft.AspNetCore.Mvc;
 
+/*
+ * [ApiController]
+[Route("[controller]")]
+public class ChatController : ControllerBase
+{
+    private readonly ChatHistoryService _chatHistory;
+    // 1. Injektáljuk a Hub-ot a controllerbe is!
+    private readonly IHubContext<ChatHub> _hubContext;
+
+    public ChatController(ChatHistoryService chatHistory, IHubContext<ChatHub> hubContext)
+    {
+        _chatHistory = chatHistory;
+        _hubContext = hubContext;
+    }
+
+    [HttpGet]
+    public IActionResult GetMessages() 
+    {
+        return Ok(_chatHistory.GetAllMessages());
+    }
+
+    // A POST végpont, ami fogadja a külső kérést
+    [HttpPost]
+    public async Task<IActionResult> PostExternalMessage([FromBody] ChatMessage message)
+    {
+        // 2. Érvényesítjük és hozzáadjuk az üzenetet a közös tárolóhoz
+        message.Timestamp = DateTime.UtcNow;
+        _chatHistory.AddMessage(message);
+
+        // 3. A HubContext segítségével kiküldjük az üzenetet minden csatlakoztatott kliensnek
+        await _hubContext.Clients.All.SendAsync("ReceiveMessage", message);
+
+        return Ok(new { Status = "Message broadcasted successfully." });
+    }
+}
+ */
 namespace ChatApp.Endpoint.Controllers
 {
     [ApiController]
